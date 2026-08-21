f83b0d208ce40c4dad7a6a2499e6fc992b09c382
René Fischer <rene@gaehn.org>
2026-08-21 21:00:15 +0200

    Trim JSON-LD test dependency vendor

--- vendor/github.com/cayleygraph/quad/voc/core/all.go
+++
@@ -1,8 +0,0 @@
-// Package core imports all well-known RDF vocabularies.
-package core
-
-import (
-	_ "github.com/cayleygraph/quad/voc/rdf"
-	_ "github.com/cayleygraph/quad/voc/rdfs"
-	_ "github.com/cayleygraph/quad/voc/schema"
-)
